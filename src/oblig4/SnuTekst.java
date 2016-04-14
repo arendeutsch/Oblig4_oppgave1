115910c30ad1e485733cf3929449fbe7ecaddc91
Aren Deutsch <arendeutsch@gmail.com>
2016-04-14 13:34:10 +0200

    versjon 0.2

--- src/oblig4/SnuTekst.java
+++ src/oblig4/SnuTekst.java
@@ -3,6 +3,7 @@ package oblig4;
 import java.util.Scanner;
 
 public class SnuTekst {
+	static int teller = 0;
 	
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
@@ -11,12 +12,14 @@ public class SnuTekst {
 		String s = input.next();
 		System.out.print("Baklengs streng: ");	
 		baklengs(s);
+		System.out.println("");
+		System.out.print("Antall tegn er: " + teller);	
 		
 		input.close();
 	}
 	
 	public static void baklengs(String tekst){
-
+		teller++;
 		if (tekst.length() <= 1)
 			System.out.print(tekst);
 		else{
